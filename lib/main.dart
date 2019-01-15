8c60e3365520d3d3435d32fe1511c77248bd2a1d
Mauricio Medrano Machicado <maumedmac@gmail.com>
2019-01-15 09:24:22 -0400

    Agregando Iconos y acciones OnTap

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class RandomWords extends StatefulWidget{
 class RandomWordsState extends State<RandomWords>{
   @override
   final _suggestion = <WordPair>[];
+  final _saved = Set<WordPair>();
   final _biggerfont = const TextStyle(fontSize: 18.0);
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -47,9 +48,23 @@ class RandomWordsState extends State<RandomWords>{
     );
   }
   Widget _buildRow(WordPair pair){
+    final alreadySaved = _saved.contains(pair);
+
     return ListTile(
       title: Text(pair.asPascalCase,
       style: _biggerfont,),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: (){
+        setState(() {
+          if(alreadySaved)
+            _saved.remove(pair);
+          else
+            _saved.add(pair);
+        });
+      },
     );
   }
 }
